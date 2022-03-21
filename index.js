88c8510b801933d7a2cff70074ffafc02f9ea252
Min <nichapha.t@reverseads.com>
2022-03-21 16:59:31 +0700

    click to copy hex code feature

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let chosenColor = document.getElementById('color-picker')
 let colorCode = ''
 
 let getColorBtn = document.getElementById('actionBtn')
+let colorScheme =[]
 
 // Choose color mode
 mode.addEventListener('change',function(){
@@ -15,27 +16,45 @@ mode.addEventListener('change',function(){
 // Choose color hex
 chosenColor.addEventListener('change',function(){
   colorCode = chosenColor.value.slice(1)
-  console.log(colorCode)
 })
 
 // Fetch the scheme
 async function getScheme() {
   const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorCode}&mode=${modeValue}&count=4`);
   const data = await response.json();
-  const colorScheme = data.colors
+   colorScheme = data.colors
+
+  //  Set id property to each color
+   for(i=0; i< colorScheme.length;i++){
+    colorScheme[i].id = i+1
+   }
+}
+
+function render(){
   const colortHtml = colorScheme.map((item,index) =>{
     return `
-    <div class='container-color-scheme' id=${index+1} onclick='show()'>
+    <div class='container-color-scheme' id=${item.id} onclick=getHex(${item.id})>
     <div class='color-scheme' style="background-color:${item.hex.value}"></div>
+    <div class='modal'> Click to copy code </div>
     <div class='hexCode'> ${item.hex.value} </div>
     </div>
     `
+})
+document.querySelector('.scheme').innerHTML = colortHtml.join('')
+}
+
+function getHex(id){
+  colorScheme.map(item=>{
+    if(item.id === id){
+      navigator.clipboard.writeText(`${item.hex.value}`)
+      console.log(item.hex.value)
+    }
   })
-  document.querySelector('.scheme').innerHTML = colortHtml.join('')
 }
 
 getColorBtn.addEventListener('click',()=>{
   getScheme()
+  render()
 })
 
 
